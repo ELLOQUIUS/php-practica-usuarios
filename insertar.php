681fa9dce2b8dd0b173b34571db93eb91b250c27
elloquius <elloquius@gmail.com>
2024-11-02 12:07:14 -0400

    Se agregan los archivos de conexion, index, menu e insert

---
+++ insertar.php
@@ -0,0 +1,42 @@
+
+    <table>
+        <thead>
+            <tr>
+                <th>Reporte de usuario</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cedula:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Nombre:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Apellido:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="email"></td>
+            </tr>
+            <tr>
+                <td>Telefono:</td>
+                <td><input type="number"></td>
+            </tr>
+            <tr>
+                <td>Direccion:</td>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Sexo:</td>
+                <td>
+                    <input type="round">
+                    <input type="round">
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
